10b6d98cad375c9266dab9093a7a5db5b7b78611
Teo Klestrup Röijezon <teo@nullable.se>
2018-01-29 13:54:33 +0100

    Changed a few fields from snake_case to camelCase

--- app/database/Job.scala
+++ app/database/Job.scala
@@ -7,8 +7,8 @@ import PgProfile.api._
 case class Job (id: Id[Job],
                 fieldId: Id[Field],
                 name: String,
-                from_date: Timestamp,
-                to_date: Option[Timestamp],
+                fromDate: Timestamp,
+                toDate: Option[Timestamp],
                 country: String) extends HasId {
   type Self = Job
   type IdType = Long
@@ -19,8 +19,8 @@ class Jobs(tag: Tag) extends Table[Job](tag, "jobs") {
   def id = column[Id[Job]]("id", O.PrimaryKey, O.AutoInc)
   def fieldId = column[Id[Field]] ("field")
   def name = column[String] ("name")
-  def from_date = column[Timestamp] ("from_date")
-  def to_date = column[Option[Timestamp]] ("to_date")
+  def fromDate = column[Timestamp] ("from_date")
+  def toDate = column[Option[Timestamp]] ("to_date")
   def country = column[String] ("country")
 
   def field = foreignKey("field_fk", fieldId, Fields)(_.id)
@@ -28,7 +28,7 @@ class Jobs(tag: Tag) extends Table[Job](tag, "jobs") {
   /**
     *
     */
-  override def * = (id, fieldId, name, from_date, to_date, country) <> (Job.tupled, Job.unapply)
+  override def * = (id, fieldId, name, fromDate, toDate, country) <> (Job.tupled, Job.unapply)
 }
 
 /**
