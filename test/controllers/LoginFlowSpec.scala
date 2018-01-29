3d6cfee02679bb4cd6162a1c04ab203a0cdf8e74
Teo Klestrup Röijezon <teo@nullable.se>
2018-01-29 12:24:47 +0100

    Made LoginFlowSpec create its own user account, instead of depending on the fixtures

--- test/controllers/LoginFlowSpec.scala
+++ test/controllers/LoginFlowSpec.scala
@@ -1,9 +1,11 @@
 package controllers
 
+import models.UserManager
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import scala.concurrent.ExecutionContext
 import utils.DbOneServerPerTest
 
 /**
@@ -14,17 +16,21 @@ import utils.DbOneServerPerTest
  */
 class LoginFlowSpec extends PlaySpec with DbOneServerPerTest with OneBrowserPerTest with FirefoxFactory with Injecting {
   "login should work" in {
+    val userManager = inject[UserManager]
+    implicit val executionContext = inject[ExecutionContext]
+    await(userManager.register("scrooge", "money"))
+
     go to (s"http://localhost:$port/")
     click on find(id("nav-login")).value
 
-    textField(name("username")).value = "donald_duck"
-    pwdField(name("password")).value = "12345"
+    textField(name("username")).value = "scrooge"
+    pwdField(name("password")).value = "moolah"
     click on find(id("login")).value
-    find(id("message")).value.text mustNot include("You have been logged in")
+    find(id("message")).fold("")(_.text) mustNot include("You have been logged in")
     find(tagName("body")).value.text must include("Invalid username or password")
 
-    textField(name("username")).value = "donald_duck"
-    pwdField(name("password")).value = "123456"
+    textField(name("username")).value = "scrooge"
+    pwdField(name("password")).value = "money"
     click on find(id("login")).value
     find(id("message")).value.text must include("You have been logged in")
   }
