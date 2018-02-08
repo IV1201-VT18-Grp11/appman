ccdfb8ec828802622b0708e3fc733138e8a52490
kimte <souri@kth.se>
2018-02-08 16:41:25 +0100

    Added the Competence table in the database.

---
+++ app/database/Competence.scala
@@ -0,0 +1,19 @@
+package database
+
+import database.PgProfile.api._
+
+case class Competence(id: Id[Competence],
+                      name: String
+                     ) extends HasId {
+  type Self = Competence
+  type IdType = Long
+}
+
+class Competences(tag: Tag) extends Table[Competence](tag, "competences") {
+  def id = column[Id[Competence]]("id", O.PrimaryKey, O.AutoInc)
+  def name = column[String]("name")
+
+  override def * = (id, name) <> (Competence.tupled, Competence.unapply)
+}
+
+object Competences extends TableQuery[Competences](new Competences(_))
