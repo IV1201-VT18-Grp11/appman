41090ed0d32e0819fa18da11e62afc0f0444a711
Xiao <hexiao125@gmail.com>
2018-02-21 16:19:32 +0100

    Added integration test for applying jobs and sending applications

--- test/controllers/JobControllerSpec.scala
+++ test/controllers/JobControllerSpec.scala
@@ -14,13 +14,13 @@ class JobControllerSpec extends PlaySpec with DbOneAppPerTest with Injecting {
   "trying to view the available job list" should {
     "show all available jobs" in {
       val request = FakeRequest(routes.JobController.jobList())
-      val page = route(app, request).get
+      val page    = route(app, request).get
 
       status(page) mustBe OK
       contentAsString(page) must (
         include("Scala Magician")
           and include(routes.JobController.jobDescription(Id[Job](1)).url)
-        )
+      )
     }
   }
 
@@ -33,13 +33,13 @@ class JobControllerSpec extends PlaySpec with DbOneAppPerTest with Injecting {
 
         status(page) mustBe OK
         contentAsString(page) must (
-          include("Scala Magician") // Name
-            and include("2018-10-01") // End date
-            and include("Sweden") // Country
+          include("Scala Magician")                                // Name
+            and include("2018-10-01")                              // End date
+            and include("Sweden")                                  // Country
             and include("Wonderful experience that will make you") // Description
-            and include("Inner beauty.") // Requirements
-            and include("Apply") // Apply link
-          )
+            and include("Inner beauty.")                           // Requirements
+            and include("Apply")                                   // Apply link
+        )
       }
     }
 
@@ -64,23 +64,60 @@ class JobControllerSpec extends PlaySpec with DbOneAppPerTest with Injecting {
         FakeRequest(routes.JobController.applyForJob(Id[Job](1)))
       val page = route(app, request).get
 
-      status(page) mustBe OK
+      status(page) mustBe SEE_OTHER
+      val infoPage =
+        route(app, FakeRequest("GET", redirectLocation(page).get)).get
+
+      status(infoPage) mustBe OK
       contentAsString(page) must (
         include("Apply to be a Scala Magician")
           and include("Why are you a good fit for this job")
           and include("Competences")
           and include("Years of Experience")
+          and include("IT")
+          and include("Cooking")
           and include("From")
           and include("To")
+          and include("Avialability begins at")
+          and include("until")
           and include("Apply")
-        )
+      )
     }
   }
 
   "trying to send a application" should {
-    "show the application details" in{
+    "show the application details" in {
       val request =
-        FakeRequest(routes.JobController.applicationDescription(Id[Job](1), Id[JobApplication](1)))
+        FakeRequest(
+          routes.JobController.applicationDescription(Id[Job](1),
+                                                      Id[JobApplication](1))
+        )
+      val page = route(app, request).get
+
+      status(page) mustBe SEE_OTHER
+      val infoPage =
+        route(app, FakeRequest("GET", redirectLocation(page).get)).get
+
+      status(infoPage) mustBe OK
+      contentAsString(infoPage) must (
+        include("Application number")
+          and include("Full Name")
+          and include("Teo Klestrup")
+          and include("Username")
+          and include("laserkitten")
+          and include("E-mail")
+          and include("teo@nullable.se")
+          and include("Job")
+          and include("Scala Magician")
+          and include("Availabilities")
+          and include("2018-01-01")
+          and include("Date of Application")
+          and include("Web, 21 Feb 2018")
+          and include("Applicant Description")
+          and include("I am awesome")
+          and include("Applicant Competence")
+          and include("IT (11.0 years")
+      )
     }
   }
 }
