c1fae8d88ba3434c20e7a0177f4a405c7b198e36
Xiao <hexiao125@gmail.com>
2018-02-21 14:43:13 +0100

    Added integration test for applying jobs

--- test/controllers/JobControllerSpec.scala
+++ test/controllers/JobControllerSpec.scala
@@ -1,25 +1,26 @@
 package controllers
 
-import database.{Id, Job}
+import database.{Id, Job, JobApplication}
 import models.ApplicationManager
 import org.scalatestplus.play.PlaySpec
 import play.api.http.HttpErrorHandler
 import play.api.test.{FakeRequest, Injecting}
 import play.api.test.Helpers._
 import utils.DbOneAppPerTest
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class JobControllerSpec extends PlaySpec with DbOneAppPerTest with Injecting {
   "trying to view the available job list" should {
     "show all available jobs" in {
       val request = FakeRequest(routes.JobController.jobList())
-      val page    = route(app, request).get
+      val page = route(app, request).get
 
       status(page) mustBe OK
       contentAsString(page) must (
         include("Scala Magician")
           and include(routes.JobController.jobDescription(Id[Job](1)).url)
-      )
+        )
     }
   }
 
@@ -32,13 +33,13 @@ class JobControllerSpec extends PlaySpec with DbOneAppPerTest with Injecting {
 
         status(page) mustBe OK
         contentAsString(page) must (
-          include("Scala Magician")                                // Name
-            and include("2018-10-01")                              // End date
-            and include("Sweden")                                  // Country
+          include("Scala Magician") // Name
+            and include("2018-10-01") // End date
+            and include("Sweden") // Country
             and include("Wonderful experience that will make you") // Description
-            and include("Inner beauty.")                           // Requirements
-            and include("Apply")                                   // Apply link
-        )
+            and include("Inner beauty.") // Requirements
+            and include("Apply") // Apply link
+          )
       }
     }
 
@@ -54,6 +55,32 @@ class JobControllerSpec extends PlaySpec with DbOneAppPerTest with Injecting {
 
         status(page) mustBe NOT_FOUND
       }
+    }
+  }
+
+  "trying to apply for a job" should {
+    "show the application form" in {
+      val request =
+        FakeRequest(routes.JobController.applyForJob(Id[Job](1)))
+      val page = route(app, request).get
+
+      status(page) mustBe OK
+      contentAsString(page) must (
+        include("Apply to be a Scala Magician")
+          and include("Why are you a good fit for this job")
+          and include("Competences")
+          and include("Years of Experience")
+          and include("From")
+          and include("To")
+          and include("Apply")
+        )
+    }
+  }
+
+  "trying to send a application" should {
+    "show the application details" in{
+      val request =
+        FakeRequest(routes.JobController.applicationDescription(Id[Job](1), Id[JobApplication](1)))
     }
   }
 }
